5e2c72919e65f146c813fe0365c932989b6c8df2
Calvin Cai <calvincai@Calvins-MacBook-Pro.local>
2024-09-22 13:15:40 -0400

    attachments complete

--- attachments.py
+++ attachments.py
@@ -1,11 +1,37 @@
-def create_attachment():
-    pass
+from tinydb import TinyDB, Query
 
-def read_attachment():
-    pass
+db = TinyDB('db.json')
+attachments_table = db.table('attachments')
+query = Query()
 
-def update_attachment():
-    pass
+def get_next_auto_id() -> int:
+    """Retrieve the next available attachment ID."""
+    existing_ids = [doc['attachment_id'] for doc in attachments_table.all()]
+    return max(existing_ids, default=-1) + 1
 
-def delete_attachment():
-    pass
+def create_attachment(meeting_id: int, attachment_url: str, attachment_id = None):
+    if attachments_table.search(query.attachment_id == attachment_id):
+        print('Attachment with id already exists')
+        return False
+    if(not meeting_id or not attachment_url):
+        return False
+    if not attachment_id:
+        attachment_id = get_next_auto_id()
+    attachments_table.insert({'attachment_id': attachment_id, 'meeting_id': meeting_id, 'attachment_url': attachment_url})
+
+def read_attachment(attachment_id=None):
+    return attachments_table.search(query.attachment_id == attachment_id) if attachment_id else attachments_table.all()
+
+def update_attachment(attachment_id: int, **kwargs):
+    if attachments_table.search(query.attachment_id == attachment_id):
+        attachments_table.update(kwargs, query.attachment_id == attachment_id)
+        return True
+    print('Attachment not found.')
+    return False
+
+def delete_attachment(attachment_id: int):
+    if attachments_table.remove(query.attachment_id == attachment_id):
+        return True
+    print('Attachment not found.')
+    return False
+
--- cli.py
+++ cli.py
@@ -3,11 +3,14 @@ from calendars import *
 from participants import *
 from attachments import *
 from tinydb import TinyDB, Query
+import click
 
 
-
+@click.command()
 def main():
-    input('What would you like to do?\n')
+    db = TinyDB('db.json')
+    
+    #input('What would you like to do?\n')
 
 if __name__ == '__main__':
     main()
