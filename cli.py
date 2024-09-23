1ff1dea3eec81e04cf8d02d4e46ba7b8d66b502b
Calvin Cai <calvincai@Calvins-MacBook-Pro.local>
2024-09-23 15:53:08 -0400

    attachment cli updated

--- cli.py
+++ cli.py
@@ -66,6 +66,62 @@ def list_calendars_cli():
 
 # END OF CALENDAR CLI COMMANDS
 
+
+# START OF ATTACHMENT CLI COMMANDS
+
+# Create an Attachment
+@cli.command()
+@click.option('--id', prompt='Attachment ID', help='Optional: ID of the attachment', default='', type = int)
+@click.option('--meeting_id', prompt='Meeting ID', help='ID of meeting associated with attachment', type = int)
+@click.option('--url', prompt='Attachment URL', help='URL of the attachment')
+def create_attachment_cli(id, meeting_id, url):
+    attachment_id = create_attachment(meeting_id, url, id)
+    print(attachment_id)
+    if attachment_id is not None:
+        click.echo(f'Attachment created with ID: {attachment_id}')
+    else:
+        click.echo('Attachment creation failed')
+
+# Read an Attachment
+@cli.command()
+@click.argument('attachment_id', type = int)
+def read_attachment_cli(attachment_id):
+    attachment = read_attachment(attachment_id)
+    if attachment:
+        click.echo(f'Attachment: {attachment}')
+    else:
+        click.echo('Attachment not found')
+
+@cli.command()
+def read_all_attachments_cli():
+    attachments = read_attachment()
+    if attachments:
+        click.echo(f'{attachments}')
+    else:
+        click.echo('No attachments found')
+
+# Update an Attachment
+@cli.command()
+@click.argument('attachment_id', type = int)
+@click.option('--meeting_id', help='New Meeting ID', type = int)
+@click.option('--url', help='New URL')
+def update_attachment_cli(attachment_id, meeting_id, url):
+    updated_attachment = update_attachment(attachment_id, meeting_id=meeting_id, attachment_url=url)
+    if updated_attachment:
+        click.echo(f'Attachment updated: {updated_attachment}')
+
+# Delete an Attachment
+@cli.command()
+@click.argument('attachment_id', type = int)
+def delete_attachment_cli(attachment_id):
+    success = delete_attachment(attachment_id)
+    if success:
+        click.echo(f'Attachment {attachment_id} deleted.')
+    else:
+        click.echo('Attachment deletion failed')
+
+# END OF ATTACHMENT CLI COMMANDS
+
 # Main entry point
 if __name__ == '__main__':
     cli()
