4b59061e7d122f624be4c5673ed58b51b38e3774
Calvin Cai <calvincai@Calvins-MacBook-Pro-2.local>
2024-10-02 11:23:08 -0400

    working backend

---
+++ backend/meetingschedulerapp/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Meetings, Calendars, Participants, Attachments
+
+class MeetingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meetings
+        fields = '__all__'
+
+class CalendarsSerializer(serializers.ModelSerializer):
+    meetings = MeetingsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Calendars
+        fields = '__all__'
+
+class ParticipantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participants
+        fields = '__all__'
+
+class AttachmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachments
+        fields = '__all__'
