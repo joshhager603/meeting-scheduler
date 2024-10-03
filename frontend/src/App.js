ec96b3a589ecc5f3884218b98163b1083665b52c
Emmanuel Makoye <makoye@Emmanuels-MacBook-Air.local>
2024-10-03 13:59:45 -0400

    removed participant view file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import Participants from './components/participant/Participants';
 import Calendars from './components/calendar/Calendars';
 import WeeklyCalendar from './components/calendar/ WeeklyCalendar';
 import Meeting from './components/meeting/Meeting';
@@ -11,11 +10,8 @@ const App = () => {
         <Router>
             <div className="App">
                 <Navbar />
-
-                {/* Main Content */}
                 <div className="container mx-auto p-4">
                     <Routes>
-                        <Route path="/participants" element={<Participants />} />
                         <Route path="/" element={<Calendars />} />
                         <Route path="/weeklyCalendar/:id" element={<WeeklyCalendar />} />
                         <Route path="/meeting/:id" element={<Meeting />} />
