fa554ceb9fbd0e5deb882acbf4ab83f0506662e7
Emmanuel Makoye <makoye@Emmanuels-Air.lan>
2024-10-19 17:01:56 -0400

    refactored apigateway

--- docker/ga3-microservices/api-gateway/startup.sh
+++ docker/ga3-microservices/api-gateway/startup.sh
@@ -1,10 +1,16 @@
-# Put commands to start gateway here
+#!/bin/bash
 
-# wait for the services to start up before starting the gateway
+# Wait for the services to start up before starting the gateway
 /wait-for-it.sh calendar-service:8001 -- echo "Calendar service is up"
 /wait-for-it.sh meeting-service:8002 -- echo "Meeting service is up"
 /wait-for-it.sh participant-service:8003 -- echo "Participant service is up"
 /wait-for-it.sh attachment-service:8004 -- echo "Attachment service is up"
 
-# put startup commands here
+# Run migrations
+echo "Running migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+# Start the Django server
 echo "Starting API gateway!"
+python manage.py runserver 0.0.0.0:8000 --noreload
